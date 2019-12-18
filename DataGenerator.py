fac173e47fa05a0a0cd8263dedea1c3706aa2ef0
Katelyn <katelyn.kuntz@yash.com>
2019-12-18 10:24:56 -0600

    Started messing with the class stuff

--- DataGenerator.py
+++ DataGenerator.py
@@ -11,25 +11,28 @@ accessKeyId = ""  # Your AWS Access Key ID
 secretAccessKey = ""  # Your AWS Secret Access Key
 inputStream = "ExampleInputStream"  # The name of the stream being used as input into the Kinesis Analytics hotspots application
 
-# Variables that control properties of the generated data.
-xRange = [0, 10]  # The range of values taken by the x-coordinate
-yRange = [0, 10]  # The range of values taken by the y-coordinate
-hotspotSideLength = 1  # The side length of the hotspot
-hotspotWeight = 0.2  # The fraction ofpoints that are draw from the hotspots
-datetime
+'# Variables that control properties of the generated data.'
+'#xRange = [0, 10]  # The range of values taken by the x-coordinate'
+'#yRange = [0, 10]  # The range of values taken by the y-coordinate'
+'#hotspotSideLength = 1  # The side length of the hotspot'
+'#hotspotWeight = 0.2  # The fraction ofpoints that are draw from the hotspots'
+
 
 device_model = "Apple watch"
 system_name = "watchOS"
 system_version = "6.1.1"
-#measurementTime
+measurementTime = str(datetime.now())  #This should change as needed
 source_type = "raw"
 data_type = "gyro"
 
 
 #xyz are randomized
-x_gyroRange = [0, 1]
-y_gyroRange = [0, 1]
-z_gyroRange = [0, 1]
+'#This needs to be randomized'
+xgyro_Range = [0, 1]
+'#this needs to be randomized'
+ygyro_Range = [0, 1]
+'#this needs to be randomized'
+zgyro_Range = [0, 1]
 
 study_type = "meal_study"
 subject_id = "7ba82a44-7c79-4d6f-94cf-5bfd678db34b"
@@ -37,11 +40,12 @@ phone_unique_id = "678328B8-E1A4-4DF4-B0FB-38673C77B70F"
 watch_unique_id = "37C6FA39-C85B-4018-9D03-0B012BE67828"
 app_version = "1.0.3"
 
-def generate_point_in_rectangle(x_min, width, y_min, height):
-    """Generate points uniformly in the given rectangle."""
+def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgyro_min, zgyro_max):
+    """Generate values between 0 and 1 ."""
     return {
-        'x': x_min + random() * width,
-        'y': y_min + random() * height
+        'xgyro': xgyro_min + random(),
+        'ygyro': ygyro_min + random(),
+        'zgyro': zgyro_min + random()
     }
 
 
@@ -50,14 +54,33 @@ class RecordGenerator(object):
     a point is drawn from a hotspot, otherwise it is drawn from the base distribution. The location of the hotspot
     changes after every 1000 points generated."""
 
+#Ok define attributes of this class
     def __init__(self):
-        self.x_min = xRange[0]
+        self.xgyro_min = xgyro_Range[0]
+        self.xgyro_max = xgyro_Range[1]
+        self.ygyro_min = ygyro_Range[0]
+        self.zgyro_min = zgyro_Range[0]
+
+       """self.x_min = xRange[0]
         self.width = xRange[1] - xRange[0]
         self.y_min = yRange[0]
         self.height = yRange[1] - yRange[0]
         self.points_generated = 0
         self.hotspot_x_min = None
-        self.hotspot_y_min = None
+        self.hotspot_y_min = None"""
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def get_record(self):
         if self.points_generated % 1000 == 0:
