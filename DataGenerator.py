55577b0a5b6a7156e7635e07ef693e30e0ffe543
tejashree16 <mundarigetejashree@gmail.com>
2019-12-18 12:06:56 -0600

    Update DataGenerator.py
    testing without secretkey and access key

--- DataGenerator.py
+++ DataGenerator.py
@@ -2,17 +2,15 @@ import boto3
 import json
 import time
 import datetime
-
 '#modified from tutorial at https://docs.aws.amazon.com/kinesisanalytics/latest/dev/app-hotspots-prepare.html'
 from random import random
-
-# TODO need to configure these securely
-
-awsRegion = ""  # The AWS region where your Kinesis Analytics application is configured.
+#cognito= boto3.client('cognito-identity')
+#response = cognito.get_credentials_for_identity(IdentityId="us-east-1_uskfYSSa6")
+# Modify this section to reflect your AWS configuration.
+awsRegion = "us-east-1"  # The AWS region where your Kinesis Analytics application is configured.
 accessKeyId = ""  # Your AWS Access Key ID
 secretAccessKey = ""  # Your AWS Secret Access Key
-inputStream = "ExampleInputStream"
-
+inputStream = "test"
 '# The name of the stream being used as input into the Kinesis Analytics hotspots application'
 
 '# Variables that control properties of the generated data.'
@@ -21,15 +19,19 @@ inputStream = "ExampleInputStream"
 '#hotspotSideLength = 1  # The side length of the hotspot'
 '#hotspotWeight = 0.2  # The fraction ofpoints that are draw from the hotspots'
 
-'measurementTime = str(datetime.now())'
+
+'#measurementTime = str(datetime.now())'
+
 
 '#xyz are randomized'
 '#This needs to be randomized'
-'#xgyro_Range = [0, 1]'
+xgyro_Range = [0, 1]
 '#this needs to be randomized'
-'#ygyro_Range = [0, 1]'
+ygyro_Range = [0, 1]
 '#this needs to be randomized'
-'#zgyro_Range = [0, 1]'
+zgyro_Range = [0, 1]
+
+
 
 
 def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgyro_min, zgyro_max):
@@ -38,7 +40,7 @@ def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgy
         'device_model': "Apple watch",
         'system_name': "watchOS",
         'system_version': "6.1.1",
-        'measurement_time': str(datetime.datetime.today()),
+
         'source_type': "raw",
         'data_type': "gyro",
         'xgyro': xgyro_min + random(),
@@ -49,6 +51,8 @@ def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgy
         'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
         'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
         'app_version': "1.0.3"
+
+
     }
 
 
@@ -58,14 +62,13 @@ class RecordGenerator(object):
     changes after every 1000 points generated."""
 
     '#Ok define attributes of this class'
-
     def __init__(self):
-        self.xgyro_min = 0
-        self.xgyro_max = 1
-        self.ygyro_min = 0
-        self.ygyro_max = 1 
-        self.zgyro_min = 0
-        self.zgyro_max = 1
+        self.xgyro_min = xgyro_Range[0]
+        self.xgyro_max = xgyro_Range[1]
+        self.ygyro_min = ygyro_Range[0]
+        self.ygyro_max = ygyro_Range[1]
+        self.zgyro_min = zgyro_Range[0]
+        self.zgyro_max = zgyro_Range[1]
         """self.x_min = xRange[0]
         self.width = xRange[1] - xRange[0]
         self.y_min = yRange[0]
@@ -110,12 +113,9 @@ def main():
     while True:
         records = generator.get_records(batch_size)
         print(records)
-        kinesis.put_records(StreamName="ExampleInputStream", Records=records)
-        # TODO change to kinesis stream name
+        kinesis.put_records(StreamName="test", Records=records)    # TODO change to kinesis stream name
 
-        '#in seconds'
         time.sleep(0.1)
-
         '#TODO the combination of time.sleep and batch size will determine how many in a minute, '
         '#and how they are spaced out'
 
