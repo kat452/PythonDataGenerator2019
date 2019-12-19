7a8b4734dc50e437a157c18fcd240f4932003155
Katelyn <katelyn.kuntz@yash.com>
2019-12-19 12:59:40 -0600

    replaced some hardcoded strings with references to the deviceConfiguration file

--- jsonDictonaries.py
+++ jsonDictonaries.py
@@ -1,22 +1,22 @@
 from random import random
 import datetime
-
+import deviceConfiguration
 
 def generate_random_meal_heartrate():
 
     return {
-        'device_model': "Apple watch",
-        'system_name': "watchOS",
-        'system_version': "6.1.1",
+        'device_model': deviceConfiguration.device_model,
+        'system_name': deviceConfiguration.system_name,
+        'system_version': deviceConfiguration.system_version,
         'source_type': "active",
         'data_type': "meal",
         'start_time': "2019-12-02T13:43:41+0000",
         'stop_time': "2019-12-02T13:43:42+0000",
         'study_type': "meal_study",
-        'subject_id' : "yashtech",
-        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
-        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
-        'app_version': "1.0"
+        'subject_id': deviceConfiguration.subject_id,
+        'phone_unique_id': deviceConfiguration.phone_unique_id,
+        'watch_unique_id': deviceConfiguration.watch_unique_id,
+        'app_version': deviceConfiguration.app_version
     }
 
 
