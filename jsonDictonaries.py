a88dabca1bca1c6676a53d3d13cdab2d989352b5
tejashree16 <mundarigetejashree@gmail.com>
2019-12-19 11:59:14 -0600

    config file changes

---
+++ jsonDictonaries.py
@@ -0,0 +1,41 @@
+def generate_random_meal():
+
+    return {
+        'device_model': "Apple watch",
+        'system_name': "watchOS",
+        'system_version': "6.1.1",
+
+        'source_type': "health",
+        'data_type': "heart_rate",
+        'heart_beat_count': 70 + random()*50,
+        'study_type': "meal_study",
+        'subject_id': "7ba82a44-7c79-4d6f-94cf-5bfd678db34b",
+        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
+        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
+        'app_version': "1.0.3"
+    }
+
+
+
+
+{
+
+"device_model":"Apple Watch", "system_name":"watchOS",
+
+"system_version":"6.1",
+
+ "source_type":"active",
+
+"data_type":“meal", 
+
+"start_time":"2019-12-02T13:43:41+0000", "stop_time":"2019-12-02T13:43:42+0000", "study_type":"meal_study",
+
+"subject_id":"yashtech",
+
+"phone_unique_id":"678328B8-E1A4-4DF4-B0FB-38673C77B70F",
+
+"watch_unique_id":"37C6FA39-C85B-4018-9D03-0B012BE67828",
+
+"app_version":"1.0“
+
+}
