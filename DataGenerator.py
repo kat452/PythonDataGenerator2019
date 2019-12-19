fa2cdbe2a50d2368e4b9df052ded55220f224a07
Katelyn <katelyn.kuntz@yash.com>
2019-12-19 11:35:13 -0600

    testing

--- DataGenerator.py
+++ DataGenerator.py
@@ -54,9 +54,18 @@ def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgy
 def generate_random_heart_rate():
 
     return {
-
-
-
+        'device_model': "Apple watch",
+        'system_name': "watchOS",
+        'system_version': "6.1.1",
+        'added_to_health': str(datetime.datetime.today()),
+        'source_type': "health",
+        'data_type': "heart_rate",
+        'heart_beat_count': 70 + random()*50,
+        'study_type': "meal_study",
+        'subject_id': "7ba82a44-7c79-4d6f-94cf-5bfd678db34b",
+        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
+        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
+        'app_version': "1.0.3"
     }
 
 
@@ -109,7 +118,7 @@ class RecordGenerator(object):
 
 def main():
     kinesis = boto3.client('kinesis', aws_access_key_id=accessKeyId, aws_secret_access_key=secretAccessKey)
-
+    #This was changed
     generator = RecordGenerator()
     batch_size = 1
     '#This was changed because of request for batch size of 100'
@@ -117,7 +126,7 @@ def main():
     while True:
         records = generator.get_records(batch_size)
         print(records)
-        kinesis.put_records(StreamName="test", Records=records)    # TODO change to kinesis stream name
+        '# kinesis.put_records(StreamName="test", Records=records)    # TODO change to kinesis stream name'
 
         '#in seconds'
         time.sleep(0.1)
