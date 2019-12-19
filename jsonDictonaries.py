1c8cc4c5382b7bf899db6cab8b579312346d2888
Katelyn <katelyn.kuntz@yash.com>
2019-12-19 13:03:33 -0600

    replaced some hardcoded strings with references to the deviceConfiguration file

--- jsonDictonaries.py
+++ jsonDictonaries.py
@@ -23,9 +23,9 @@ def generate_random_meal_heartrate():
 def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgyro_min, zgyro_max):
     """Generate values between 0 and 1 ."""
     return {
-        'device_model': "Apple watch",
-        'system_name': "watchOS",
-        'system_version': "6.1.1",
+        'device_model': deviceConfiguration.device_model,
+        'system_name': deviceConfiguration.system_name,
+        'system_version': deviceConfiguration.system_version,
         'measurement_time': str(datetime.datetime.today()),
         'source_type': "raw",
         'data_type': "gyro",
@@ -33,37 +33,37 @@ def generate_random_gyro_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgy
         'ygyro': ygyro_min + random(),
         'zgyro': zgyro_min + random(),
         'study_type': "meal_study",
-        'subject_id': "7ba82a44-7c79-4d6f-94cf-5bfd678db34b",
-        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
-        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
-        'app_version': "1.0.3"
+        'subject_id': deviceConfiguration.subject_id,
+        'phone_unique_id': deviceConfiguration.phone_unique_id,
+        'watch_unique_id': deviceConfiguration.watch_unique_id,
+        'app_version': deviceConfiguration.app_version
     }
 
 
 def generate_random_heart_rate():
 
     return {
-        'device_model': "Apple watch",
-        'system_name': "watchOS",
-        'system_version': "6.1.1",
+        'device_model': deviceConfiguration.device_model,
+        'system_name': deviceConfiguration.system_name,
+        'system_version': deviceConfiguration.system_version,
         'added_to_health': str(datetime.datetime.today()),
         'source_type': "health",
         'data_type': "heart_rate",
         'heart_beat_count': 70 + random()*50,
         'study_type': "meal_study",
-        'subject_id': "7ba82a44-7c79-4d6f-94cf-5bfd678db34b",
-        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
-        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
-        'app_version': "1.0.3"
+        'subject_id': deviceConfiguration.subject_id,
+        'phone_unique_id': deviceConfiguration.phone_unique_id,
+        'watch_unique_id': deviceConfiguration.watch_unique_id,
+        'app_version': deviceConfiguration.app_version
     }
 
 
 def generate_random_accelerometer_details(xgyro_min, xgryo_max, ygyro_min, ygyro_max, zgyro_min, zgyro_max):
 
     return{
-        'device_model': "Apple watch",
-        'system_name': "watchOS",
-        'system_version': "6.1.1",
+        'device_model': deviceConfiguration.device_model,
+        'system_name': deviceConfiguration.system_name,
+        'system_version': deviceConfiguration.system_version,
         'measurement_time': str(datetime.datetime.today()),
         'source_type': "raw",
         'data_type': "accelerometer",
@@ -71,10 +71,10 @@ def generate_random_accelerometer_details(xgyro_min, xgryo_max, ygyro_min, ygyro
         'ygyro': ygyro_min + random(),
         'zgyro': zgyro_min + random(),
         'study_type': "meal_study",
-        'subject_id': "7ba82a44-7c79-4d6f-94cf-5bfd678db34b",
-        'phone_unique_id': "678328B8-E1A4-4DF4-B0FB-38673C77B70F",
-        'watch_unique_id': "37C6FA39-C85B-4018-9D03-0B012BE67828",
-        'app_version': "1.0.3"
+        'subject_id': deviceConfiguration.subject_id,
+        'phone_unique_id': deviceConfiguration.phone_unique_id,
+        'watch_unique_id': deviceConfiguration.watch_unique_id,
+        'app_version': deviceConfiguration.app_version
 
     }
 
